f5487c5ffb3bb7f6db3441400ed5e94b66f881c0
rtemerario <rebeccatemerario@gmail.com>
2022-06-26 22:02:36 -0400

    Updated weather units to imperial

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function displayWeatherCondition(response) {
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
